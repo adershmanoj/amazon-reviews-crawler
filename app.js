05e76fc8065c7fe79946425661bc903ce15d2c9b
adershmanoj <adershmanoj@gmail.com>
2018-01-14 12:05:38 +0530

    Updated app.js to fetch multiple review pages

--- app.js
+++ app.js
@@ -1,23 +1,38 @@
 'use strict'
-const Nightmare = require('nightmare')
+var Nightmare = require('nightmare'),
+  nightmare = Nightmare(),
+  vo = require('vo'); // to pipeline multiple requests
 const randomUa = require('random-ua')
 const evalFunction = require('amazon-reviews-crawler-eval')
+
 const defaultOptions = {
-	page: 'https://www.amazon.com/product-reviews/{{asin}}/ref=cm_cr_arp_d_viewopt_srt?reviewerType=all_reviews&pageNumber=1&sortBy=recent',
+	page: 'https://www.amazon.com/product-reviews/{{asin}}/ref=cm_cr_arp_d_viewopt_srt?reviewerType=all_reviews&pageNumber={{pageNumber}}&sortBy=recent',
 	stopAtReviewId: false
 }
 
 module.exports = (asin, opt) => {
 	return new Promise((resolve, reject) => {
 		opt = Object.assign({}, defaultOptions, opt)
-		new Nightmare({
-				//show: true, openDevTools: { mode: 'detach' }
-			})
-			.useragent(opt.userAgent || randomUa.generate())
-			.goto(opt.page.replace('{{asin}}', asin))
-			.evaluate(evalFunction, opt)
-			.end(obj => obj)
-			.then(resolve)
-			.catch(reject)
+        opt.page = opt.page.replace('{{asin}}', asin);
+        var title;
+        var run = function*(){
+          var results = [];
+          for(var i=1; i<5; i++){
+            var result = yield nightmare.useragent(randomUa.generate()).goto(opt.page.replace('{{pageNumber}}', i)).evaluate(evalFunction, opt);
+            results = results.concat(result.reviews); 
+            title = result.title;
+          }
+          return results;
+        }
+        vo(run)(function(err, results){
+          resolve({'title': title, 'reviews': results})
+          reject(err)
+        });
+//		new Nightmare()
+//			.useragent(randomUa.generate())
+//			.goto(opt.page.replace('{{pageNumber}}', 1))
+//			.evaluate(evalFunction, opt)
+//			.then(resolve)
+//			.catch(reject)
 	})
 }
